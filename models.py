dc87c9fe520ff574f958884fd050eedcdd29d0d3
PremjeeSingh <74917881+PremjeeSingh@users.noreply.github.com>
2020-11-24 17:40:59 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,24 +1,13 @@
-from django.contrib.auth.models import Permission, User
+
+
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
-class Album(models.Model):
-    user = models.ForeignKey(User, default=1)
-    artist = models.CharField(max_length=250)
-    album_title = models.CharField(max_length=500)
-    genre = models.CharField(max_length=100)
-    album_logo = models.FileField()
-    is_favorite = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.album_title + ' - ' + self.artist
-
-
-class Song(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    song_title = models.CharField(max_length=250)
-    audio_file = models.FileField(default='')
-    is_favorite = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.song_title
+class Question(models.Model):
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
