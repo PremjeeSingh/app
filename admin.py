cdc632f29b1514f85a8bc040b3fb8cd6f891bb30
PremjeeSingh <74917881+PremjeeSingh@users.noreply.github.com>
2020-11-24 16:36:03 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Album, Song
 
-admin.site.register(Album)
-admin.site.register(Song)
+from .models import Question
+
+admin.site.register(Question)
